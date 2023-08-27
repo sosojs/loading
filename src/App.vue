f9e918cda200fec11cbebbbffc48f0b0eba17cf8
unionTOR <3095421243@qq.com>
2023-08-27 21:01:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import Edit from "./components/Edit.vue";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+// TODO: 列表渲染
+//思路：  声明响应式list  调用接口额获取数据  后端数据赋给list  绑定到table组件
+const list = ref([]);
+const getList = async () => {
+  //接口调用
+  const res = await axios.get("/list");
+  // 交给list
+  list.value = res.data;
+};
+onMounted(() => getList());
+
+// TODO: 删除功能
+//思路获取当前行的id;通过id调用删除接口   更新最新的列表
+const onDelete = async (id) => {
+  console.log(id);
+  await axios.delete(`/del/${id}`);
+  getList();
+};
+
+// TODO: 编辑功能
+</script>
+
+<template>
+  <div class="app">
+    <el-table :data="list">
+      <el-table-column label="ID" prop="id"></el-table-column>
+      <el-table-column label="姓名" prop="name" width="150"></el-table-column>
+      <el-table-column label="籍贯" prop="place"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template #default="{ row }">
+          <el-button type="primary" link>编辑</el-button>
+          <el-button type="danger" @click="onDelete(row.id)" link
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+  <Edit />
+</template>
+
+<style scoped>
+.app {
+  width: 980px;
+  margin: 100px auto 0;
+}
+</style>
